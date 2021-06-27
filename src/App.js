aecea415a3c30c328728b6efdca9a44960be7582
tingyunchiu <c199403@gmail.com>
2021-06-28 00:52:15 +0800

    add custom hook

--- src/App.js
+++ src/App.js
@@ -1,52 +1,26 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState} from 'react';
+import useGetNewsApi from'./useGetNewsApi.js'
 import SearchBox from './searchbox.js'
 import Table from './table.js'
 
 function App() {
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [items, setItems] = useState([]);
   const [search, setSearch] = useState('');
   const [currentPage, setCurrentPage] = useState(0);
-  const [nbPages,setNbPages] = useState(0);
 
   const onSearchChange = (e) => {
-    if (e !== search){
-      setSearch(e)
-      setIsLoaded(false)
-      setItems([]);
-      setCurrentPage(0)
-      setNbPages(0)
-    }
+    setSearch(e)
+    setCurrentPage(0)
   }
 
   const onDelete = (id) => {
-    setItems(items.filter(item => item.objectID !== id))
+    delItems(items.filter(item => item.objectID !== id))
   }
 
   const onLoadMoreClicked =() => {
       setCurrentPage(currentPage+1)
-      setIsLoaded(false)
   }
 
-  useEffect(() => {
-    fetch(`https://hn.algolia.com/api/v1/search?page=${currentPage}&hitsPerPage=10&query=${search}`)
-      .then(res => res.json())
-      .then(
-        (result) => {
-          setIsLoaded(true);
-          setItems(items => [...items, ...result.hits]);
-          setNbPages(result.nbPages)
-          },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          setIsLoaded(true);
-          setError(error);
-        }
-      )
-  }, [search,currentPage])
+  let [error, isLoaded, items, nbPages, delItems] = useGetNewsApi(search,currentPage)
 
 
   if (error) {
---
+++ src/useGetNewsApi.js
@@ -0,0 +1,46 @@
+import { useState, useEffect, useRef, useCallback} from "react";
+
+const useGetNewsApi = (search,currentPage) => {
+  const isFirstRender = useRef(true);
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const prevSearchRef = useRef('');
+  const [items, setItems] = useState([]);
+  const [nbPages,setNbPages] = useState(0);
+
+  useEffect(() => {
+    setIsLoaded(false)
+    if (prevSearchRef.current!==search) {
+      setItems([])
+      setNbPages(0)
+    }
+    if(!isFirstRender.current){
+      fetch(`https://hn.algolia.com/api/v1/search?page=${currentPage}&hitsPerPage=10&query=${search}`)
+        .then(res => res.json())
+        .then(
+          (result) => {
+            setIsLoaded(true);
+            setItems(items => [...items, ...result.hits]);
+            setNbPages(result.nbPages)
+            prevSearchRef.current = search
+            },
+          // Note: it's important to handle errors here
+          // instead of a catch() block so that we don't swallow
+          // exceptions from actual bugs in components.
+          (error) => {
+            setIsLoaded(true);
+            setError(error);
+            prevSearchRef.current = search
+          }
+        )
+    }else{
+      isFirstRender.current = false
+    }
+  }, [search,currentPage])
+
+  const delItems = useCallback(function(i) { setItems(i); }, []);
+
+  return [error, isLoaded, items, nbPages, delItems];
+};
+
+export default useGetNewsApi;
