12766b5db48e6632e182b50d729eed86f55f2710
tingyunchiu <c199403@gmail.com>
2021-07-03 17:17:21 +0800

    add keys to list

--- src/components/table.js
+++ src/components/table.js
@@ -2,30 +2,28 @@ import React from 'react';
 import Row from './row.js'
 
 function Table({items, onDelete}) {
-const rows = [];
-items.forEach(item => {
-	rows.push(
-		<Row item = {item}
-		key={item.objectID}
-		onDelete = {onDelete}/>
-	)
-})
-return(
-	<div style = {{width: '100%', overflowX: 'auto'}}>
-        <table style = {{width: '100%', tableLayout: 'fixed'}}>
-            <thead style={{backgroundColor: "grey"}}>
-              <tr>
-                <th style={{width: '15%', wordBreak: 'break-all'}}>ID</th>
-                <th style={{width: '15%', wordBreak: 'break-all'}}>Author</th>
-                <th style={{width: '15%', wordBreak: 'break-all'}}>Comments</th>
-                <th style={{width: '20%', wordBreak: 'word-break'}}>Title</th>
-                <th style = {{wordBreak: 'break-all'}}>URL</th>
-                <th style={{width: '65px'}}>Remove</th>
-              </tr>
-            </thead>
-            <tbody>{rows}</tbody>
-        </table>
-    </div>
-);
+    const rows = items.map(item =>
+    	<Row item = {item}
+    		key={item.objectID}
+    		onDelete = {onDelete}/>
+    )
+
+    return(
+    	<div style = {{width: '100%', overflowX: 'auto'}}>
+            <table style = {{width: '100%', tableLayout: 'fixed'}}>
+                <thead style={{backgroundColor: "grey"}}>
+                  <tr>
+                    <th style={{width: '15%', wordBreak: 'break-all'}}>ID</th>
+                    <th style={{width: '15%', wordBreak: 'break-all'}}>Author</th>
+                    <th style={{width: '15%', wordBreak: 'break-all'}}>Comments</th>
+                    <th style={{width: '20%', wordBreak: 'word-break'}}>Title</th>
+                    <th style = {{wordBreak: 'break-all'}}>URL</th>
+                    <th style={{width: '65px'}}>Remove</th>
+                  </tr>
+                </thead>
+                <tbody>{rows}</tbody>
+            </table>
+        </div>
+    );
 }
 export default Table;
